6a45772e4203878fb3421080755fcee001da08dd
Archie <yangyxcn@gmail.com>
2014-10-22 21:58:27 +0800

    Initial Commit

---
+++ TaskMeTests/TaskMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  TaskMeTests.swift
+//  TaskMeTests
+//
+//  Created by archie on 14/10/22.
+//  Copyright (c) 2014年 archie. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TaskMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
